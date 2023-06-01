1bf44c7399359f9db2667155c66db1c231ff9ccf
fakhriden1221 <fakhriden.1002@gmail.com>
2023-06-01 19:18:15 +0300

    deleted:    templatemo_553_xtra_blog/about.html 	deleted:    templatemo_553_xtra_blog/contact.html 	deleted:    templatemo_553_xtra_blog/css/bootstrap.min.css 	deleted:    templatemo_553_xtra_blog/fontawesome/LICENSE.txt 	deleted:    templatemo_553_xtra_blog/fontawesome/css/all.min.css 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-brands-400.eot 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-brands-400.svg 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-brands-400.ttf 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-brands-400.woff 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-brands-400.woff2 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-regular-400.eot 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-regular-400.svg 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-regular-400.ttf 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-regular-400.woff 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-regular-400.woff2 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-solid-900.eot 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-solid-900.svg 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-solid-900.ttf 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-solid-900.woff 	deleted:    templatemo_553_xtra_blog/fontawesome/webfonts/fa-solid-900.woff2 	deleted:    templatemo_553_xtra_blog/img/about-01.jpg 	deleted:    templatemo_553_xtra_blog/img/about-02.jpg 	deleted:    templatemo_553_xtra_blog/img/about-03.jpg 	deleted:    templatemo_553_xtra_blog/img/about-04.jpg 	deleted:    templatemo_553_xtra_blog/img/about-05.jpg 	deleted:    templatemo_553_xtra_blog/img/comment-1.jpg 	deleted:    templatemo_553_xtra_blog/img/comment-2.jpg 	deleted:    templatemo_553_xtra_blog/img/img-02.jpg 	deleted:    templatemo_553_xtra_blog/img/img-03.jpg 	deleted:    templatemo_553_xtra_blog/img/img-04.jpg 	deleted:    templatemo_553_xtra_blog/img/img-05.jpg 	deleted:    templatemo_553_xtra_blog/img/img-06.jpg 	deleted:    templatemo_553_xtra_blog/index.html 	new file:   templatemo_553_xtra_blog/index.php 	deleted:    templatemo_553_xtra_blog/js/jquery.min.js 	deleted:    templatemo_553_xtra_blog/post.html 	deleted:    templatemo_553_xtra_blog/video/wheat-field.mp4

---
+++ templatemo_553_xtra_blog/index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xtra Blog</title>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> <!-- https://fonts.google.com/ --> 
+    <link href="css/templatemo-xtra-blog.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header class="tm-header" id="tm-header">
+        <div class="tm-header-wrapper">
+            <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="tm-site-header">
+                <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-times fa-2x"></i></div>
+                <h1 class="text-center">Xtra Blog</h1>
+            </div>
+            <nav class="tm-nav" id="tm-nav">
+                <ul>
+                    <li class="tm-nav-item active"><a href="index.php" class="tm-nav-link">
+                            <i class="fas fa-home"></i>
+                            Blog Home
+                        </a></li>
+                    <li class="tm-nav-item"><a href="index.php" class="tm-nav-link">
+                            <i class="fas fa-pen"></i>
+                            Single Post
+                        </a></li>
+                    <li class="tm-nav-item"><a href="index.php" class="tm-nav-link">
+                            <i class="fas fa-users"></i>
+                            About Xtra
+                        </a></li>
+                    <li class="tm-nav-item"><a href="index.php" class="tm-nav-link">
+                            <i class="far fa-comments"></i>
+                            Contact Us
+                        </a></li>
+                </ul>
+            </nav>
+           
+    </header>
+    <div class="container-fluid">
+        <main class="tm-main">
+            <!-- Search form -->
+            <div class="row tm-row">
+                <div class="col-12">
+                    <form method="GET" class="form-inline tm-mb-80 tm-search-form">
+                        <input class="form-control tm-search-input" name="query" type="text" placeholder="Search..." aria-label="Search">
+                        <button class="tm-search-button" type="submit">
+                            <i class="fas fa-search tm-search-icon" aria-hidden="true"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <!--job opportunities-->
+            <div class="row tm-row">
+                <article class="col-12 col-md-6 tm-post">
+                    <hr class="tm-hr-primary">
+                    <a href="index.php" class="effect-lily tm-post-link tm-pt-60">
+                        <div class="tm-post-link-inner">
+                            <img src="img/img-01.jpg" alt="Image" class="img-fluid">
+                        </div>
+                        <span class="position-absolute tm-new-badge">New</span>
+                        <h2 class="tm-pt-30 tm-color-primary tm-post-title">Simple and useful HTML layout</h2>
+                    </a>
+                    <p class="tm-pt-30">
+                        There is a clickable image with beautiful hover effect and active title link for each post item.
+                        Left side is a sticky menu bar. Right side is a blog content that will scroll up and down.
+                    </p>
+                    <div class="d-flex justify-content-between tm-pt-45">
+                        <span class="tm-color-primary">Travel . Events</span>
+                        <span class="tm-color-primary">June 24, 2020</span>
+                    </div>
+                    <hr>
+                    <div class="d-flex justify-content-between">
+                        <span>36 comments</span>
+                        <span>by Admin Nat</span>
+                    </div>
+                </article>
+            </div>
+            <!--navigation between job opportunities-->
+            <div class="row tm-row tm-mt-100 tm-mb-75">
+                <div class="tm-prev-next-wrapper">
+                    <a href="#" class="mb-2 tm-btn tm-btn-primary tm-prev-next disabled tm-mr-20">Prev</a>
+                    <a href="#" class="mb-2 tm-btn tm-btn-primary tm-prev-next">Next</a>
+                </div>
+                <div class="tm-paging-wrapper">
+                    <span class="d-inline-block mr-3">Page</span>
+                    <nav class="tm-paging-nav d-inline-block">
+                        <ul>
+                            <li class="tm-paging-item active">
+                                <a href="#" class="mb-2 tm-btn tm-paging-link">1</a>
+                            </li>
+                            <li class="tm-paging-item">
+                                <a href="#" class="mb-2 tm-btn tm-paging-link">2</a>
+                            </li>
+                            <li class="tm-paging-item">
+                                <a href="#" class="mb-2 tm-btn tm-paging-link">3</a>
+                            </li>
+                            <li class="tm-paging-item">
+                                <a href="#" class="mb-2 tm-btn tm-paging-link">4</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <!--footer-->
+            <footer class="row tm-row">
+                <hr class="col-12">
+                <div class="col-md-6 col-12 tm-color-gray">
+                    Design: <a rel="nofollow" target="_parent" href="https://templatemo.com" class="tm-external-link">TemplateMo</a>
+                </div>
+                <div class="col-md-6 col-12 tm-color-gray tm-copyright">
+                    Copyright 2020 Xtra Blog Company Co. Ltd.
+                </div>
+            </footer>
+        </main>
+    </div>
+
+    <!-- <script src="js/templatemo-script.js"></script>
+
+-->
+</body>
+
+</html>
